621d04d9b6a0bff138023e1cd45a4389bbad3960
Rahul <rahulrajeev800@.com>
2023-01-16 23:35:50 +0100

    feat:

--- api/index.js
+++ api/index.js
@@ -1,1 +1,19 @@
-console.log("hello world!")
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log('backend server is running!');
+
+});
