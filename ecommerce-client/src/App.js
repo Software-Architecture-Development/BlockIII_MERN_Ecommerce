a56d698bd898f02b5a5e04d2197bc5cb064d8817
Shubham <82511219+sheriyy@users.noreply.github.com>
2023-01-16 23:43:10 +0100

    Folder structure for frontend

--- ecommerce-client/src/App.js
+++ ecommerce-client/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <a  
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
