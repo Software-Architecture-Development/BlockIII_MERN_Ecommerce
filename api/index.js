8ba5b79249f762749f1f61d47951e16157931c8f
mohamedjaved0205 <119460980+mohamedjaved0205@users.noreply.github.com>
2023-01-19 22:24:12 +0100

    feat: Add routes

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
+const userRoute = require ("./routes/user")
 dotenv.config();
 
 //mongodb configuration
@@ -12,6 +12,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+  app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log('backend server is running!');
