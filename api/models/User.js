386988cc19fcbcc6617cedce397feedabd783f31
Rahul <rahulrajeev800@.com>
2023-01-23 19:53:53 +0100

    feat: add User/Product model

--- api/models/User.js
+++ api/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-//CREATING SCHEMA
+//CREATING SCHEMA OF USER
 const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
