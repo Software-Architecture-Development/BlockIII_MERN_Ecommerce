58527118a144d4a63de8259ff7828c202d9720e2
Rahul <rahulrajeev800@.com>
2023-01-23 19:45:42 +0100

    feat: add models

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoute = require("./routes/user")
 
 dotenv.config();
 
@@ -12,6 +13,9 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use(express.json());
+app.use("/test", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log('backend server is running!');
