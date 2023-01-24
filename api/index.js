4557f1fee446cd19d6410ca6fdb270e356752bb0
Rahul <rahulrajeev800@.com>
2023-01-24 02:46:12 +0100

    fix: hash password

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const userRoute = require("./routes/user")
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -15,7 +16,8 @@ mongoose
   });
 
 app.use(express.json());
-app.use("/test", userRoute);
+app.use("/auth", authRoute);
+// app.use("/auth", authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log('backend server is running!');
---
+++ api/routes/auth.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+// HASHING PASSWORD
+const CryptoJS = require("crypto-js");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SECRET
+      ).toString(),
+    });
+  
+    try {
+      const savedUser = await newUser.save();
+      res.status(201).json(savedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  module.exports = router;
