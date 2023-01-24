a1fb65152f9d7fb36bf3a576fdc52131fd97bfce
mohamedjaved0205 <119460980+mohamedjaved0205@users.noreply.github.com>
2023-01-24 01:33:42 +0100

    feat: Add Auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,14 +1,22 @@
-const router = require("express").Router;
-const User = require("../models/Users")
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 //REGISTER
-router.post("/register",(req, res)=>{
+router.post("/register", async (req, res) => {
     const newUser = new User({
-        username:req.body.username,
-        email:req.body.email,
-        password:req.body.password,
+      username: req.body.username,
+      email: req.body.email,
+      password: CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+      ).toString(),
     });
-});
-
-
-module.exports = router;
+  
+    try {
+      const savedUser = await newUser.save();
+      res.status(201).json(savedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
