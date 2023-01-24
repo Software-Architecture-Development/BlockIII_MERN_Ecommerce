dc5576aa6fa3b326e5680d4de12a2e5f7728b6b9
Rahul <rahulrajeev800@.com>
2023-01-25 00:16:59 +0100

    fix: delete user

--- api/routes/user.js
+++ api/routes/user.js
@@ -29,4 +29,14 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
