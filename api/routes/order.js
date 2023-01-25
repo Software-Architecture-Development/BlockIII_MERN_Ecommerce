8d9278dde12d7158e0dfba39ec0c1a2a05dad45a
mohamedjaved0205 <119460980+mohamedjaved0205@users.noreply.github.com>
2023-01-25 17:53:02 +0100

    fix: Get monthly income

--- api/routes/order.js
+++ api/routes/order.js
@@ -56,7 +56,6 @@ router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
   });
   
 //GET ALL
-  
   router.get("/", verifyTokenAndAdmin, async (req, res) => {
     try {
       const orders = await Order.find();
@@ -65,5 +64,33 @@ router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+// GET MONTHLY INCOME
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+  const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+
+  try {
+    const income = await Order.aggregate([
+      { $match: { createdAt: { $gte: previousMonth } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+          sales: "$amount",
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: "$sales" },
+        },
+      },
+    ]);
+    res.status(200).json(income);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
