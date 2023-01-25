cfb7058252270855494fb57415e1ff5eafc48a24
Rahul <rahulrajeev800@.com>
2023-01-25 02:31:26 +0100

    feat: JWT CRUD cart

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -1,3 +1,48 @@
 const Cart = require("../models/Cart");
-
+const {
+    verifyToken,
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin,
+  } = require("./verifyToken");
+  
+  const router = require("express").Router();
+  
+//CREATE CART
+  
+  router.post("/", verifyToken, async (req, res) => {
+    const newOrder = new Order(req.body);
+  
+    try {
+      const savedOrder = await newOrder.save();
+      res.status(200).json(savedOrder);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+//UPDATE CART
+  router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+      const updatedOrder = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedOrder);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  //DELETE CART
+  router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+      await Order.findByIdAndDelete(req.params.id);
+      res.status(200).json("Order has been deleted...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 module.exports = router;
