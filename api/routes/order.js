c012e809d63841c1c3f9d936bccffc928e786fe8
Rahul <rahulrajeev800@.com>
2023-01-25 02:47:52 +0100

    fix: GET  order

--- api/routes/order.js
+++ api/routes/order.js
@@ -45,4 +45,25 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+//GET USER ORDERS
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+      const orders = await Order.find({ userId: req.params.userId });
+      res.status(200).json(orders);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+//GET ALL
+  
+  router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    try {
+      const orders = await Order.find();
+      res.status(200).json(orders);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
