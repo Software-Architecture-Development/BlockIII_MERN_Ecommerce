0614b05558c3d0493ffe68074790d6a56eec4573
Shubham <82511219+sheriyy@users.noreply.github.com>
2023-01-25 23:55:12 +0100

    Carousel component with sliding and transition effect

--- client-ecommerce/src/components/Carousel.jsx
+++ client-ecommerce/src/components/Carousel.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
 import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
+import { carouselItems } from '../data';
 
 const Container = styled.div`
 width: 100%;
@@ -13,8 +14,6 @@ overflow: hidden;
 const Arrow = styled.div`
 width: 50px;
 height: 50px;
-background-color: #fff7f7;
-border-radius: 50%;
 display: flex;
 align-items: center;
 justify-content: center;
@@ -23,21 +22,24 @@ top: 0;
 bottom: 0;
 left: ${(props) => props.direction === "left" && "15px"};
 right: ${(props) => props.direction === "right" && "15px"};
-margin: auto;
 cursor: pointer;
-opacity: 0.5;
-z-index: 2;
+opacity: 0.7;
+z-index: 1;
+background-color: lightgray;
+border-radius: 50%;
+margin: auto;
 `
 const Wrapper =styled.div`
 height: 100%;
 display: flex;
+transform: translateX(${(props) => props.carouselIndex * -100}vw);
+transition: all 1.5s ease;
 `
 const Slide = styled.div`
 width: 100vw;
-height: 100vh;
 display: flex;
 align-items: center;
-background-color: #${(props) => props.bg};
+height: 100vh;
 `
 const ImageContainer = styled.div`
 height: 100%;
@@ -53,7 +55,7 @@ const DetailsContainer = styled.div`
 const Title = styled.h1`
   font-size:70px;
 `
-const Descr = styled.p`
+const Description = styled.p`
 margin: 50px 0;
 font-size:20px;
 font-weight:light;
@@ -65,24 +67,36 @@ background-color:transparent;
 cursor: pointer;
 `
 const Carousel = () => {
+const [carouselIndex, setCarouselIndex] =useState(0);
+
+ const handleClick = (imgDirection) =>{
+     if(imgDirection ==="right"){
+      setCarouselIndex(carouselIndex <3 ? carouselIndex +1 : 0)
+     } else{
+      setCarouselIndex(carouselIndex > 0 ? carouselIndex -1 : 3 )
+     }
+  }
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick ={() => handleClick("left")}>
          <ArrowLeftOutlinedIcon/>
       </Arrow>
-      <Wrapper>
-        <Slide>
-         <ImageContainer>
-            <Image src="https://cdn.pixabay.com/photo/2017/08/10/08/00/shirt-2619788_1280.jpg"/>
-         </ImageContainer>
-         <DetailsContainer>
-            <Title>SALE</Title>
-            <Descr>AMAZING 30% DISCOUNT ON NEW ARRIVALS!</Descr>
-            <Button>SHOP NOW</Button>
-         </DetailsContainer>
-         </Slide>       
+      <Wrapper carouselIndex ={carouselIndex}>
+        {carouselItems.map((item) =>(
+          <Slide bg={item.bg}>
+            {console.log(item.bg)}
+          <ImageContainer>
+          <Image src= {item.img}/>
+          </ImageContainer>
+          <DetailsContainer>
+          <Title>{item.title}</Title>
+          <Description>{item.desc}</Description>
+          <Button>SHOP NOW</Button>
+          </DetailsContainer>
+          </Slide>     
+        ))}   
       </Wrapper>
-      <Arrow  direction="right">
+      <Arrow  direction="right" onClick ={() => handleClick("right")}>
          <ArrowRightOutlinedIcon/>
       </Arrow>
     </Container>
