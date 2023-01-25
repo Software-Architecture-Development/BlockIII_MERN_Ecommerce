d6ccca021102a57f5959296f7cb0991a82844855
Rahul <rahulrajeev800@.com>
2023-01-25 02:43:53 +0100

    fix: GET user cart

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -36,11 +36,31 @@ const {
     }
   });
   
-  //DELETE CART
+//DELETE CART
   router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
     try {
       await Order.findByIdAndDelete(req.params.id);
       res.status(200).json("Order has been deleted...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+//GET USER CART
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+      const cart = await Cart.findOne({ userId: req.params.userId });
+      res.status(200).json(cart);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+//GET ALL
+  router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    try {
+      const carts = await Cart.find();
+      res.status(200).json(carts);
     } catch (err) {
       res.status(500).json(err);
     }
