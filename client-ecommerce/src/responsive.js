a4250d5dd76d43f50ff5a32619c1fe4715e8620b
Shubham <82511219+sheriyy@users.noreply.github.com>
2023-01-26 16:59:08 +0100

    Product component having list of trending products

--- client-ecommerce/src/responsive.js
+++ client-ecommerce/src/responsive.js
@@ -2,8 +2,8 @@ import { css } from "styled-components";
 
 export const mobile = (props) => {
   return css`
-    @media only screen and (max-width: 380px) {
-      ${props}
-    }
+  @media only screen and (max-width: 380px) {
+    ${props}
+  }
   `;
 };
