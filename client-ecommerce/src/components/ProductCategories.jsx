9e944712d4b28d292751b0fa972e320fc7661311
Shubham <82511219+sheriyy@users.noreply.github.com>
2023-01-26 18:31:44 +0100

    Home & product category components latest changes

--- client-ecommerce/src/components/ProductCategories.jsx
+++ client-ecommerce/src/components/ProductCategories.jsx
@@ -12,7 +12,7 @@ const ProductCategories = () => {
   return (
     <Container>
       {categories.map((item) =>(
-        <CategoryItem item={item}/>
+        <CategoryItem  key={item.id} item={item}/>
       ))}
     </Container>
   )
--- client-ecommerce/src/pages/Home.jsx
+++ client-ecommerce/src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Offers from '../components/Offers'
 import Navbar from '../components/Navbar'
+import Carousel from '../components/Carousel'
+import ProductCategories from '../components/ProductCategories'
+import Products from '../components/Products'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
+      <Offers/>
       <Navbar/>
+      <Carousel/>
+      <ProductCategories/>
+      <Products/>
+      <Contact/>
+      <Footer/>
     </div>
   )
 }
