d8823b7da0debe0fad49518650994b01b84e427b
Shubham <82511219+sheriyy@users.noreply.github.com>
2023-01-26 01:57:01 +0100

    Dummy data for project

--- client-ecommerce/src/data.js
+++ client-ecommerce/src/data.js
@@ -2,28 +2,24 @@ export const carouselItems = [
     {
       id: 1,
       img: "https://cdn.pixabay.com/photo/2017/08/10/08/00/shirt-2619788_1280.jpg",
-      bg: "blue",
       title: "AUTUMN SALE",
       desc: "GET FLAT 40% OFF FOR NEW ARRIVALS.",
     },
     {
       id: 2,
       img: "https://cdn.pixabay.com/photo/2014/10/22/17/22/relaxed-498245_1280.jpg",
-      bg: "f5fafd",
       title: "SPRING WEAR",
       desc: "GET FLAT 40% OFF FOR NEW ARRIVALS.",
     },
     {
       id: 3,
       img: "https://cdn.pixabay.com/photo/2014/05/21/14/54/feet-349687_1280.jpg",
-      bg: "fcf1ed",
       title: "SUMMER LOVE",
       desc: "GET FLAT 40% OFF FOR NEW ARRIVALS.",
     },
     {
         id: 4,
         img: "https://cdn.pixabay.com/photo/2017/08/01/01/33/beanie-2562646_1280.jpg",
-        bg: "fcf1ed",
         title: "WINTER TREND",
         desc: "GET FLAT 40% OFF FOR NEW ARRIVALS.",
       },
