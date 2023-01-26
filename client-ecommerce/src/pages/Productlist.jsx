c42daa4e72dd3feb5cdee501eb733b331b49d7a0
gauthammanjunath <gauthammanjunath007@gmail.com>
2023-01-26 19:06:36 +0100

    Added Cart

---
+++ client-ecommerce/src/pages/Productlist.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const container =styled.div;
+
+const Productlist = () => {
+
+  return (
+    <continer>
+        <navbar/>
+        
+    </continer>
+  )
+}
