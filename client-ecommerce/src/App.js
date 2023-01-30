6626318a7ba3d1754a43dd29dc6a1b2376512e99
Shubham <82511219+sheriyy@users.noreply.github.com>
2023-01-30 19:36:51 +0100

    Changes made in package json

--- client-ecommerce/src/App.js
+++ client-ecommerce/src/App.js
@@ -1,10 +1,18 @@
-import Home from './pages/Home';
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
