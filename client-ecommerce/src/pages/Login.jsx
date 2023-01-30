d90fe51759d38226928b2929a7d4fb7ac6f79a78
Rahul <rahulrajeev800@.com>
2023-01-30 21:34:50 +0100

    feat: add Login apiCalls

--- client-ecommerce/src/pages/Login.jsx
+++ client-ecommerce/src/pages/Login.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import { useState } from "react";
 import styled from "styled-components";
-import {mobile} from "../responsive";
+import { login } from "../redux/apiCalls";
+import { mobile } from "../responsive";
+import { useDispatch, useSelector } from "react-redux";
 
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
   background: linear-gradient(
-    rgba(250, 250, 250, 0.7),
-    rgba(250, 250, 250, 0.7)
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
     ),
     url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
       center;
   background-size: cover;
-  display: grid;
-  place-items: center;
-//  justify-items: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Wrapper = styled.div`
@@ -23,15 +25,6 @@ const Wrapper = styled.div`
   background-color: white;
   ${mobile({ width: "75%" })}
 `;
-
-// const Wrapper1 = styled.div`
-//   width: 45%;
-//   padding: 20px;
-//   background-color: white;
-//   display: grid;
-//   place-items: center;
-//   ${mobile({ width: "75%" })}
-// `;
 
 const Title = styled.h1`
   font-size: 24px;
@@ -54,10 +47,14 @@ const Button = styled.button`
   width: 40%;
   border: none;
   padding: 15px 20px;
-  background-color: #777799;
+  background-color: teal;
   color: white;
   cursor: pointer;
   margin-bottom: 10px;
+  &:disabled {
+    color: green;
+    cursor: not-allowed;
+  }
 `;
 
 const Link = styled.a`
@@ -67,17 +64,40 @@ const Link = styled.a`
   cursor: pointer;
 `;
 
+const Error = styled.span`
+  color: red;
+`;
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isFetching, error } = useSelector((state) => state.user);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    login(dispatch, { username, password });
+  };
   return (
     <Container>
       <Wrapper>
         <Title>SIGN IN</Title>
         <Form>
-          <Input placeholder="username" />
-          <Input placeholder="password" />
-          <Button>LOGIN</Button>
-          <Link>Forgot Password?</Link>
-          <Link>Don't have an account? Sign Up</Link>
+          <Input
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            placeholder="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button onClick={handleClick} disabled={isFetching}>
+            LOGIN
+          </Button>
+          {error && <Error>Something went wrong...</Error>}
+          <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
         </Form>
       </Wrapper>
     </Container>
