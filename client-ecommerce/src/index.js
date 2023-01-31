6b3e6b66c0992dbc9242ffc4f9321a2f41a85735
Rahul <rahulrajeev800@.com>
2023-01-31 17:27:37 +0100

    fix: persist login user

--- client-ecommerce/src/index.js
+++ client-ecommerce/src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import  store from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
      <Provider store={store}>
+     <PersistGate loading={null} persistor={persistor}>
       <App />
     </Provider>
   </React.StrictMode>
