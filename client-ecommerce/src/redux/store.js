fad6299518a958df5edf066c0f484267789fdee8
Monisha12398 <monswim12398@gmail.com>
2023-01-31 23:10:51 +0530

    deleted files

--- client-ecommerce/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import cartReducer from "./cartRedux";
-
-export default configureStore({
-    reducer:{
-        cart: cartReducer,
-    }
-})
