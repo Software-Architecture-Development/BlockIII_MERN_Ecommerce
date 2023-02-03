6e374435e0c713378913f3129c5e9c5cfe2dc333
Shubham <82511219+sheriyy@users.noreply.github.com>
2023-02-03 13:51:49 +0100

    Api call for register

--- client-ecommerce/src/redux/userRedux.js
+++ client-ecommerce/src/redux/userRedux.js
@@ -19,8 +19,12 @@ const userSlice = createSlice({
       state.isFetching = false;
       state.error = true;
     },
+    registerSuccess: (state, action) => {
+      state.isFetching = false;
+      state.currentUser = action.payload;
+    },
   },
 });
 
-export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
+export const { loginStart, loginSuccess, loginFailure, registerSuccess } = userSlice.actions;
 export default userSlice.reducer;
