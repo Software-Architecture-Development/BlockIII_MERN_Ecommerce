cecd0e7c0547201d59350bb03d16a1c376771a6a
Shubham <82511219+sheriyy@users.noreply.github.com>
2023-02-03 03:34:38 +0100

    Changes made in productlist

--- client-ecommerce/src/redux/apiCalls.js
+++ client-ecommerce/src/redux/apiCalls.js
@@ -1,5 +1,6 @@
 import { loginFailure, loginStart, loginSuccess } from "./userRedux";
 import { publicRequest } from "../requestMethods";
+import { getProductFailure, getProductStart, getProductSuccess } from "./productRedux";
 
 export const login = async (dispatch, user) => {
   dispatch(loginStart());
@@ -8,5 +9,16 @@ export const login = async (dispatch, user) => {
     dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure());
+  }     
+};
+
+
+export const getProducts = async (dispatch) => {
+  dispatch(getProductStart());
+  try {
+    const res = await publicRequest.get("/products");
+    dispatch(getProductSuccess(res.data));
+  } catch (err) {
+    dispatch(getProductFailure());
   }     
 };
