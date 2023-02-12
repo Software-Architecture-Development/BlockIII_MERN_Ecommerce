97acb8edb596790e71d70623c79d6d73a876061f
mohamedjaved0205 <119460980+mohamedjaved0205@users.noreply.github.com>
2023-02-12 22:30:19 +0100

    Fix:update in testing order

--- api/routes/test order.js
+++ api/routes/test order.js
@@ -39,3 +39,63 @@ describe("PUT /orders/:id", () => {
     const updatedOrder = {
       amount: 200,
     };
+    const response = await request(app)
+    .put(`/orders/${newOrder._id}`)
+    .set("Authorization", `Bearer ${token}`)
+    .send(updatedOrder)
+    .expect(200);
+
+  expect(response.body).toMatchObject(updatedOrder);
+});
+});
+
+describe("DELETE /orders/:id", () => {
+it("should delete an order", async () => {
+  const newOrder = new Order({
+    userId: "123456789",
+    amount: 100,
+  });
+  await newOrder.save();
+
+  const response = await request(app)
+    .delete(`/orders/${newOrder._id}`)
+    .set("Authorization", `Bearer ${token}`)
+    .expect(200);
+
+  expect(response.body).toBe("Order has been deleted...");
+});
+});
+
+describe("GET /orders/find/:userId", () => {
+it("should get user orders", async () => {
+  const newOrder = new Order({
+    userId: "123456789",
+    amount: 100,
+  });
+  await newOrder.save();
+
+  const response = await request(app)
+    .get("/orders/find/123456789")
+    .set("Authorization", `Bearer ${token}`)
+    .expect(200);
+
+  expect(response.body).toEqual([newOrder]);
+});
+});
+
+describe("GET /orders", () => {
+it("should get all orders", async () => {
+  const newOrder = new Order({
+    userId: "123456789",
+    amount: 100,
+  });
+  await newOrder.save();
+
+  const response = await request(app)
+    .get("/orders")
+    .set("Authorization", `Bearer ${token}`)
+   
+
+
+
+
