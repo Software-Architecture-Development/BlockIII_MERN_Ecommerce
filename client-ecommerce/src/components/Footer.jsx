7f2289ccb7a493d9952d7b64973c03a583c60ae2
Shubham <82511219+sheriyy@users.noreply.github.com>
2023-02-12 19:00:06 +0100

    Responsiveness on mobile screen

--- client-ecommerce/src/components/Footer.jsx
+++ client-ecommerce/src/components/Footer.jsx
@@ -5,11 +5,14 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import FmdGoodIcon from '@mui/icons-material/FmdGood';
+import { mobile } from '../responsive';
 import CallIcon from '@mui/icons-material/Call';
 import MailIcon from '@mui/icons-material/Mail';
 
+
 const Container =styled.div`
   display:flex;
+  ${mobile({ flexDirection: "column" })}
 `
 const Logo =styled.h1`
 `
@@ -40,6 +43,7 @@ padding:20px;
 const Right =styled.div`
 flex:1;
 padding:20px;
+${mobile({ backgroundColor: "aliceblue" })}
 `
 const ContactDetails =styled.div`
  margin:20px 0;
@@ -52,6 +56,7 @@ width:50%;
 const Center =styled.div`
 flex:1;
 padding:20px;
+${mobile({ display: "none" })}
 `
 const Title= styled.h3`
 margin-bottom:20px
