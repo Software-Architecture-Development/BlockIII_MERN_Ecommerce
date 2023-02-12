3b418d5d1126f4fb4bd10b50b37d368e0c170cd9
mohamedjaved0205 <119460980+mohamedjaved0205@users.noreply.github.com>
2023-02-12 22:10:50 +0100

    fix:update in user file

--- api/models/User.js
+++ api/models/User.js
@@ -1,20 +1,11 @@
 const mongoose = require("mongoose");
 
 //CREATING SCHEMA OF USER
-<<<<<<< HEAD
 const userSchema = new mongoose.Schema(
   {
     Username: { type: String, required: true, unique: true },
-=======
-const UserSchema = new mongoose.Schema(
-  { 
-    name: { type: String, required: true, unique: true },
-    lastname: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
->>>>>>> 255e3b7b7f46d1294059e05e0b579833315aecd6
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    confirm_password: { type: String, required: true },
     isAdmin: {
       type: Boolean,
       default: false,
