c08ed37c6c45c020cd07fc486652216cdd3ff796
mohamedjaved0205 <119460980+mohamedjaved0205@users.noreply.github.com>
2023-02-13 00:01:29 +0100

    fix:testing pay

--- client-ecommerce/src/test pay.jsx
+++ client-ecommerce/src/test pay.jsx
@@ -11,3 +11,20 @@ describe("Pay component", () => {
     const button = getByText("Pay Now");
     expect(button).toBeInTheDocument();
   });
+  it("calls the correct endpoint with correct data when making a payment", async () => {
+    const { getByText } = render(<Pay />);
+    const button = getByText("Pay Now");
+    fireEvent.click(button);
+
+    const mockData = { tokenId: "12345", amount: 2000 };
+    const mockResponse = { data: "Payment successful" };
+
+    axios.post.mockResolvedValue(mockResponse);
+
+    await new Promise((resolve) => setImmediate(resolve));
+    expect(axios.post).toHaveBeenCalledWith(
+      "http://localhost:5000/api/checkout/payment",
+      mockData
+    );
+  });
+});
